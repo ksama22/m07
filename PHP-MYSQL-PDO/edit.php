7d242287f5bab42ff8ce882f79f948f8c97317e9
ksama22 <ksama22@jaumebalmes.net>
2022-10-18 20:38:19 +0200

    get data on form

--- PHP-MYSQL-PDO/edit.php
+++ PHP-MYSQL-PDO/edit.php
@@ -1,19 +1,21 @@
 <?php
-include 'conection.php';
 if (isset($_GET['id'])) {
     $myid = $_GET['id'];
-    $query = "SELECT * PRODUCTS  WHERE ID =$myid";
+
+    include 'conection.php';
+    $query = "SELECT * FROM products WHERE ID =$myid";
     $mysqlquery = mysqli_query($connection, $query);
-    $nom = $mysqlquery['name'];
-    $descripcio = $mysqlquery['description'];
-    $preu = $mysqlquery['price'];
+    ?>
 
-}
+    <?php foreach ($mysqlquery as $i => $product) { ?>
+        <h1>UPDATE ID: <?php echo $product['id'] ?></h1>
+        <form method="post" action="add_product.php">
+            <p>Name: <input type="text" name="name" value="<?php echo $product['name'] ?>"/></p>
+            <p>Description: <input type="text" name="description" value="<?php echo $product['description'] ?>"/></p>
+            <p>Price: <input type="number" name="price" value="<?php echo $product['price'] ?>"/></p>
+            <p><input type="submit"/></p>
+        </form>
 
-?>
-<form method="post" action="edit_product.php">
-    <p>Name: <input type="text" name="name" value=<?php $nom ?>></p>
-    <p>Description: <input type="text" name="description" value=<?php $descripcio ?>></p>
-    <p>Price: <input type="number" name="price" value=<?php $preu ?>/></p>
-    <p><input type="submit"/></p>
-</form>
+
+    <?php } ?>
+<?php } ?>
