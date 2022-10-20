bfa241757cc9b77ebb8eb3c529a2e22957a0e56c
MitchelRos <mrosado19@jaumebalmes.net>
2022-10-20 21:03:34 +0200

    editar los edit.php y añadir el sumit

--- PHP-MYSQL-PDO/edit.php
+++ PHP-MYSQL-PDO/edit.php
@@ -9,13 +9,27 @@ if (isset($_GET['id'])) {
 
     <?php foreach ($mysqlquery as $i => $product) { ?>
         <h1>UPDATE ID: <?php echo $product['id'] ?></h1>
-        <form method="post" action="add_product.php">
+        <form method="_GET" action="edit.php">
             <p>Name: <input type="text" name="name" value="<?php echo $product['name'] ?>"/></p>
             <p>Description: <input type="text" name="description" value="<?php echo $product['description'] ?>"/></p>
             <p>Price: <input type="number" name="price" value="<?php echo $product['price'] ?>"/></p>
-            <p><input type="submit"/></p>
+            <p><input type="submit" name="submit" value="submit"/></p>
         </form>
 
 
     <?php } ?>
 <?php } ?>
+<?php
+if (isset($_GET['submit'])) {
+    $myid = $_GET['id'];
+    $nombre = $_GET['name'];
+    $description = $_GET['dedescriptions'];
+    $price = $_GET['price'];
+    $update = $_GET['update'];
+
+    include 'conection.php';
+    $query2 = "UPDATE products SET name='$nombre', description='$description' WHERE id =$myid";
+    echo $query2," hey no funciona!";
+    $mysqlquery = mysqli_query($connection, $query2);
+}
+?>
