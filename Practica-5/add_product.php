9b5167fe8c9d395c5ff9cc4c51e463bb89aa0404
MitchelRos <mrosado19@jaumebalmes.net>
2022-11-03 19:56:51 +0100

    Correccion Mayus product

--- Practica-5/add_product.php
+++ Practica-5/add_product.php
@@ -5,15 +5,17 @@ include 'db_connection_pdo.php';
 $name = $_GET['name'];
 $description = $_GET['description'];
 $price = $_GET['price'];
+$q_sold= $_GET['q_sold'];
+
 try {
     //Crica la conexio i prepara una query
-    $stmt = $connexio->prepare("INSERT INTO Products (name,description, price) VALUES (:name,:description,:price)");
+    $stmt = $connexio->prepare("INSERT INTO products (name,description, price, q_sold) VALUES (:name,:description,:price,:q_sold)");
 
     //$stmt->bindParam(':id', $id); // la id es autoincrement, no fa falta
     $stmt->bindParam(':name', $name); // Se enlaza a la variable
     $stmt->bindParam(':description', $description); // Se enlaza a la variable
     $stmt->bindParam(':price', $price); // Se enlaza a la variable
-
+    $stmt->bindParam(':q_sold', $q_sold); // Se enlaza a la variable
     //Executa la comanda amb els valos relacionats amb bindParam
     $stmt->execute();
     #Retorna al index.php
